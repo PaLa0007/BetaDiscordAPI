454fc870ca57abd44ddd8ed4c50e7cba9e229712
Велизар Василев <stu2001261058@uni-plovdiv.bg>
2024-12-27 13:53:05 +0200

    first commit

---
+++ src/main/java/com/beta_discord_api/betadiscordapi/BetadiscordapiApplication.java
@@ -0,0 +1,13 @@
+package com.beta_discord_api.betadiscordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetadiscordapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetadiscordapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beta_discord_api/betadiscordapi/BetadiscordapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beta_discord_api.betadiscordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetadiscordapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
